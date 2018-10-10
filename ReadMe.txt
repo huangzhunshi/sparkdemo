bed41740943b165e7332e810ff2426d30485b015
yihui <123>
2018-10-10 15:16:50 +0800

    提交相关代码

--- ReadMe.txt
+++ ReadMe.txt
@@ -23,3 +23,8 @@ spark-shell --master spark://host:port
 3.服务器上执行jar包命令 (本机mysql驱动包路径 /mywork/lib/mysql/mysql-connector-java/5.1.46/mysql-connector-java-5.1.46.jar)
 bin/spark-submit --Class com.xxx.app --jars mysql驱动包路径（hive需要选） --master local[2] xxx.jar
 
+
+4.本地hive和spark路径
+hivie 路径  /usr/local/Cellar/Hive/apache-hive-2.3.3-bin/
+spark 路径  /usr/local/Cellar/Spark/spark-2.3.1-bin-hadoop2.7/
+
--- src/main/scala/so/dian/sqlapp/SparkSessionDemo.scala
+++ src/main/scala/so/dian/sqlapp/SparkSessionDemo.scala
@@ -4,9 +4,18 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSessionDemo {
   def main(args: Array[String]): Unit = {
-    val spark=SparkSession.builder().appName("sparksession").master("local[2]").getOrCreate()
+    val spark=SparkSession.builder().appName("sparksession").master("local[2]").enableHiveSupport().getOrCreate()
+    //读取josn
     val table= spark.read.json("file:///mywork/yihui.json")
     table.show()
+
+    //读取hive
+    spark.sqlContext.sql("use testdb")
+    val df=spark.sqlContext.sql("select * from stulog")
+    df.show()
+
     spark.stop()
+
+
   }
 }
