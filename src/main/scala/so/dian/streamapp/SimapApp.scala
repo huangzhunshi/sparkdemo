90ea8bf01b18428f257cdce993fb4872305f9315
yihui <123>
2018-10-15 16:40:01 +0800

    实时计算wordcount demo

--- src/main/scala/so/dian/streamapp/SimapApp.scala
+++ src/main/scala/so/dian/streamapp/SimapApp.scala
@@ -15,15 +15,29 @@ object SimapApp {
 
   def main(args: Array[String]): Unit = {
      val conf=new SparkConf().setAppName("SimapApp").setMaster("local[2]")
-     val ssc=new StreamingContext(conf,Seconds(1))
+     val ssc=new StreamingContext(conf,Seconds(5))
 
     val lines=ssc.socketTextStream("localhost",9997,StorageLevel.MEMORY_AND_DISK_SER)
 
-    //lines.flatMap(x=>x.split(" ")).map(x=>(x,1)).reduceByKey(_+_).print()
+    //获取数据并打印
+    lines.flatMap(x=>x.split(" ")).map(x=>(x,1)).reduceByKey(_+_).print()
 
-    // TODO: 窗口函数，每隔5秒计算10秒的数据 
-    lines.flatMap(x=>x.split(" ")).map(x=>(x,1))
-      .reduceByKeyAndWindow(((x:Int,y:Int)=>x+y),Seconds(10),Seconds(5)).print()
+    //循环获取数据并持久化
+//     val aa=lines.flatMap(x=>x.split(" ")).map(x=>(x,1)).reduceByKey(_+_)
+//    aa.foreachRDD(x=>{
+//
+//      x.foreachPartition(b=>{
+//        b.foreach(c=>{
+//
+//          println(c)
+//        })
+//
+//      })
+//    })
+
+    //  窗口函数，每隔5秒计算10秒的数据
+//    lines.flatMap(x=>x.split(" ")).map(x=>(x,1))
+//      .reduceByKeyAndWindow(((x:Int,y:Int)=>x+y),Seconds(10),Seconds(5)).print()
 
 
     //lines.print()
