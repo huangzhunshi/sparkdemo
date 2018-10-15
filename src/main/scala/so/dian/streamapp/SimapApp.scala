2329e1a1c9694ade8366b0c2455fe6a4f7d1d728
yihui <123>
2018-10-15 16:21:31 +0800

    实时计算wordcount demo

--- src/main/scala/so/dian/streamapp/SimapApp.scala
+++ src/main/scala/so/dian/streamapp/SimapApp.scala
@@ -15,11 +15,16 @@ object SimapApp {
 
   def main(args: Array[String]): Unit = {
      val conf=new SparkConf().setAppName("SimapApp").setMaster("local[2]")
-     val ssc=new StreamingContext(conf,Seconds(3))
+     val ssc=new StreamingContext(conf,Seconds(1))
 
     val lines=ssc.socketTextStream("localhost",9997,StorageLevel.MEMORY_AND_DISK_SER)
 
-    lines.flatMap(x=>x.split(" ")).map(x=>(x,1)).reduceByKey(_+_).print()
+    //lines.flatMap(x=>x.split(" ")).map(x=>(x,1)).reduceByKey(_+_).print()
+
+    // TODO: 窗口函数，每隔5秒计算10秒的数据 
+    lines.flatMap(x=>x.split(" ")).map(x=>(x,1))
+      .reduceByKeyAndWindow(((x:Int,y:Int)=>x+y),Seconds(10),Seconds(5)).print()
+
 
     //lines.print()
 
